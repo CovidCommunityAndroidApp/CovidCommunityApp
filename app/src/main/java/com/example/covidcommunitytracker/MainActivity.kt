e03206d0291fe206e6bb4c7abebc8a925828b37e
chris <trage002@csusm.edu>
2021-11-13 07:59:47 -0800

    Version1

---
+++ app/src/main/java/com/example/covidcommunitytracker/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.covidcommunitytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFrag = HomeFragment()
+        val mapFrag = MapFragment()
+        val searchFrag = SearchFragment()
+        val infoFrag = InfoFragment()
+
+        setCurrentFragment(homeFrag)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(homeFrag)
+                R.id.map->setCurrentFragment(mapFrag)
+                R.id.search->setCurrentFragment(searchFrag)
+                R.id.info->setCurrentFragment(infoFrag)
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+}
