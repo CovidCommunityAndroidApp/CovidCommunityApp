c0c4ccc0ebf9e444b2ef9d8df935bf3b8804a40b
chris <trage002@csusm.edu>
2021-12-07 17:33:23 -0800

    Version2: Added Yelp API for Recycler View

---
+++ app/src/main/java/com/example/covidcommunitytracker/YelpDataClasses.kt
@@ -0,0 +1,18 @@
+package com.example.covidcommunitytracker
+
+import com.google.gson.annotations.SerializedName
+
+data class YelpSearchResult (
+    @SerializedName("total") val total: Int,
+    @SerializedName("businesses") val business: List<YelpBusiness>
+
+    )
+data class YelpBusiness(
+    @SerializedName("name") val name: String,
+    val location: YelpLocation,
+    @SerializedName("image_url") val imageUrl: String
+)
+
+data class YelpLocation(
+    @SerializedName("address1") val address: String
+)
